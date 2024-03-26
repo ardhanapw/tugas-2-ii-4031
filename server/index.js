0d5fe06f483a997e94b9fd90d37c1d70a6259e47
ardhanapw <ardhanaputrawibowo@gmail.com>
2024-03-26 11:58:28 +0700

    added vercel blob dependency

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
+const upload = multer()
 
 const app = express()
 
