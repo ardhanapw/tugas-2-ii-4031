cbe6cc8e8904765b4ea757c9fb6e83767452bb23
ardhanapw <ardhanaputrawibowo@gmail.com>
2024-03-26 10:40:08 +0700

    no multer

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
+//const multer  = require('multer')
+//const upload = multer({ dest: 'uploads/' })
 
 const app = express()
 /*
@@ -11,19 +11,20 @@ let count = 15;
 */
 
 const port = 5000
-app.get('/', function (req, res) {
+app.use('/', function (req, res) {
   // req.file is the `avatar` file
   // req.body will hold the text fields, if there were any
   res.send("Server menyala")
   })
   
+/*
 app.post('/upload', upload.single('file'), function (req, res, next) {
     // req.file is the `avatar` file
     // req.body will hold the text fields, if there were any
     console.log(req.file, req.body)
   })
-
+*/
 app.listen(port, () => {
     console.log("Server menyala")
-  })
+})
 
