acfadfbf3bd9b1d9df3ad9043155ed3812705589
ardhanapw <ardhanaputrawibowo@gmail.com>
2024-03-30 23:19:12 +0700

    Membuat endpoint
    1. /upload untuk upload file asli
    2. /tofile untuk upload file hasil enkripsi/dekripsi

--- server/index.js
+++ server/index.js
@@ -1,32 +1,71 @@
 const express = require('express')
-const path = require('path')
 const multer  = require('multer')
-const upload = multer()
+const fs = require('fs')
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+      cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+      cb(null, file.originalname);
+  }
+});
+
+const upload = multer({storage: storage, preservePath: true})
 
 const app = express()
 
 const cors = require('cors');
-// Allow all origins
 app.use(cors());
-let count = 15;
 
 
 const port = 5000
 
-const { url } = await put('articles/blob.txt', 'Hello World!', { access: 'public' });
-app.use('/', function (req, res) {
-  // req.file is the `avatar` file
-  // req.body will hold the text fields, if there were any
-  res.send(process.cwd())
+app.post('/tofile', upload.single('file'), function (req, res, next){  
+  const filename = req.body.data[0].split(".")[0]
+  const extension = req.body.data[0].split(".")[1]
+  let filestream = req.body.data[1] //array of unicode in one string
+  const mode = req.body.data[2]
 
-  })
+  const destinationFilePath = 'uploads/' + mode + "/" + filename + "-" + mode + "." + extension
   
+  let processedFilestream = filestream.split(",") //array of unicode
+  
+  if(mode === "decrypt"){
+    for(let i = 0; i < processedFilestream.length; i++){
+      processedFilestream[i] = String.fromCharCode(processedFilestream[i]) //stringified array of unicode
+    }
 
-app.post('/upload', upload.single('file'), function (req, res, next) {
-    // req.file is the `avatar` file
-    // req.body will hold the text fields, if there were any
-    console.log(req.file, req.body)
-  })
+    fs.writeFile(destinationFilePath, processedFilestream.join(""), 'binary', err => {
+      if (err) {
+        console.error(err)
+        return
+      }
+    })
+  }else if(mode === "encrypt"){
+    fs.writeFile(destinationFilePath, processedFilestream.join(","), 'binary', err => {
+      if (err) {
+        console.error(err)
+        return
+      }
+    })
+  }
+
+
+})
+
+app.post('/upload', upload.single('file') ,function (req, res, next) {
+  const filePath = 'uploads/' + req.file.originalname; 
+
+  fs.readFile(filePath, 'binary', (err, data) => { 
+    if (err) { 
+      console.error('Error reading file:', err); 
+      return; 
+    } 
+    const content = data; 
+    res.send(content)
+  }) 
+})
 
 app.listen(port, () => {
     console.log("Server menyala")
